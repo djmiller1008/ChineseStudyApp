68bcbf46da9e81da3cf577b023937217e5b45f0b
David <djmiller1008@yahoo.com>
2022-06-09 19:57:03 +0800

    added passport middleware to express app

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require("./routes/api/users");
 
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -9,8 +12,17 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
