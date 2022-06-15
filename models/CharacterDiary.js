a2091260908568d547bd5b99f464a3f1cdf68c94
David <djmiller1008@yahoo.com>
2022-06-15 11:24:09 +0800

    Add CharacterDiary model

---
+++ models/CharacterDiary.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CharacterDiarySchema = new Schema({
+    userId: {
+        type: Number,
+        required: true
+    },
+    character: {
+        type: String,
+        required: true
+    },
+    definition: {
+        type: String,
+        required: true
+    },
+    pinyin: {
+        type: String,
+        required: true
+    }
+  }, {
+    timestamps: true
+})
+
+module.exports = CharacterDiary = mongoose.model('CharacterDiary', CharacterDiarySchema);
+
