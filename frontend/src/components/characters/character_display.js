1ac98df8cd14868aaeff6277c64af19d5d529186
David <djmiller1008@yahoo.com>
2022-06-15 11:04:20 +0800

    Add character display component

--- frontend/src/components/characters/character_display.js
+++ frontend/src/components/characters/character_display.js
@@ -1,18 +1,28 @@
 import React from "react";
+import "../../assets/character.scss";
 
-class CharacterDisplay extends React.Component {
-    constructor(props) {
-        super(props);
+const CharacterDisplay = (props) => {
 
-    }
-
-    
-    render() {
-        return (
-            <div>{this.props.location.state.word.string}</div>
-        )
-    }
-
+    return (
+        <div className="main-character-display">
+            <div className="character-display-div">
+                <section className="character-section">
+                    {props.location.state.word.string}
+                </section>
+                <section className="character-info-section">
+                    <p>{props.location.state.word.kDefinition}</p>
+                    <p>{props.location.state.word.kMandarin}</p>
+                </section>
+            </div>
+            <section className="button-section">
+                <button className="add-character-button">Add to Character Diary</button>
+            </section>
+           
+        </div>
+        
+    )
 }
+
+
 
 export default CharacterDisplay;
