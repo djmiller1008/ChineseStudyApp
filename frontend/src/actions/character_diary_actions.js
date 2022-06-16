e74a96e63b9f799f798057125455765b967c93fa
David <djmiller1008@yahoo.com>
2022-06-16 12:31:00 +0800

    Add redux diary action creators

---
+++ frontend/src/actions/character_diary_actions.js
@@ -0,0 +1,26 @@
+import * as APIUtil from '../util/character_diary_api_util';
+
+export const RECEIVE_NEW_ENTRY = 'RECEIVE_NEW_ENTRY';
+export const RECEIVE_USER_DIARY = 'RECEIVE_USER_DIARY';
+
+export const receiveNewEntry = (entry) => ({
+    type: RECEIVE_NEW_ENTRY,
+    entry
+});
+
+export const receiveUserDiary = (diary) => ({
+    type: RECEIVE_USER_DIARY,
+    diary
+});
+
+export const addEntryToDiary = entry => dispatch => (
+    APIUtil.addCharacterToDiary(entry)
+        .then(entry => dispatch(receiveNewEntry(entry.data)))
+        .catch(err => console.log(err))
+);
+
+export const getUserDiary = id => dispatch => (
+    APIUtil.getDiary(id)
+        .then(diary => dispatch(receiveUserDiary(diary.data)))
+        .catch(err => console.log(err))
+);
