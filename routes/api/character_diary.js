844e42f317cf96f0883f7b0724cc62184165088f
David <djmiller1008@yahoo.com>
2022-06-16 12:29:41 +0800

    Add diary routes to get all entries and post a new entry

---
+++ routes/api/character_diary.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const passport = require('passport');
+const CharacterDiary = require('../../models/CharacterDiary');
+
+
+const router = express.Router();
+
+
+
+router.get('/user/:user_id', (req, res) => {
+    CharacterDiary.find({ userId: req.params.user_id})
+        .then(characters => res.json(characters))
+        .catch(err =>
+            res.status(404).json({ noCharactersFound: 'There are no characters in your diary'}
+            )
+        );
+});
+
+
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        
+        const newCharacterDiary = new CharacterDiary({
+            character: req.body.character,
+            userId: req.body.userId,
+            definition: req.body.definition,
+            pinyin: req.body.pinyin
+        });
+
+        newCharacterDiary.save().then(character => res.json(character));
+    }
+);
+
+module.exports = router;
+
+
+
+
