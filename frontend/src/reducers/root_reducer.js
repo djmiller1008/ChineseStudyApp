b97ed4b1c9dfb0d3949a9cc14f2b5fab4fb94234
David <djmiller1008@yahoo.com>
2022-06-16 12:37:39 +0800

    Add diary reducer to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
+import characterDiaryReducer from "./character_diary_reducer";
 import errorsReducer from "./errors_reducer";
 import sessionReducer from "./session_api_reducer";
 
 
 const rootReducer = combineReducers({
     session: sessionReducer,
-    errors: errorsReducer
+    errors: errorsReducer,
+    diary: characterDiaryReducer
 });
 
 export default rootReducer;
