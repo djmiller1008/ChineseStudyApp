c1b6b0b4821009aa824c248ba4a50af9c2915b3d
David <djmiller1008@yahoo.com>
2022-06-19 00:07:30 +0800

    Debugging

--- frontend/src/actions/character_diary_actions.js
+++ frontend/src/actions/character_diary_actions.js
@@ -2,6 +2,12 @@ import * as APIUtil from '../util/character_diary_api_util';
 
 export const RECEIVE_NEW_ENTRY = 'RECEIVE_NEW_ENTRY';
 export const RECEIVE_USER_DIARY = 'RECEIVE_USER_DIARY';
+export const REMOVE_ENTRY = 'REMOVE_ENTRY';
+
+export const removeEntry = (entry) => ({
+    type: REMOVE_ENTRY,
+    entry
+})
 
 export const receiveNewEntry = (entry) => ({
     type: RECEIVE_NEW_ENTRY,
@@ -12,6 +18,12 @@ export const receiveUserDiary = (diary) => ({
     type: RECEIVE_USER_DIARY,
     diary
 });
+
+export const removeEntryFromDiary = id => dispatch => (
+    APIUtil.removeCharacterFromDiary(id)
+        .then((entry) => dispatch(removeEntry(entry.data)))
+        .catch(err => console.log(err))
+);
 
 export const addEntryToDiary = entry => dispatch => (
     APIUtil.addCharacterToDiary(entry)
