c2b369cd26f201dfb519faae6303b5c054c17c5e
David <djmiller1008@yahoo.com>
2022-06-19 00:10:49 +0800

    Add props to get id and remove entry dispatch function

--- frontend/src/components/characters/character_display_container.js
+++ frontend/src/components/characters/character_display_container.js
@@ -1,17 +1,22 @@
 import { connect } from 'react-redux';
 import CharacterDisplay from './character_display';
-import { addEntryToDiary } from '../../actions/character_diary_actions';
+import { addEntryToDiary, getUserDiary, removeEntryFromDiary } from '../../actions/character_diary_actions';
 
-const mapStateToProps = ({ session }) => ({
-    userId: session.user.id
+const mapStateToProps = ({ session, diary }) => ({
+    user_id: session.user.id,
+    diary: Object.values(diary).map(word => word.character),
+    diaryWithIds: Object.values(diary).map(word => word)
 })
 
 const mapDispatchToProps = dispatch => ({
-    addEntryToDiary: (entry) => dispatch(addEntryToDiary(entry))
+    addEntryToDiary: (entry) => dispatch(addEntryToDiary(entry)),
+    getDiary: (id) => dispatch(getUserDiary(id)),
+    removeEntry: (id) => dispatch(removeEntryFromDiary(id))
 });
 
 export default connect(
     mapStateToProps,
     mapDispatchToProps
 )(CharacterDisplay)
+
 
