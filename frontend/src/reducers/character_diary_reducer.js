a5c2001c78c1798047be4519037ae839d9b4ae72
David <djmiller1008@yahoo.com>
2022-06-19 00:13:00 +0800

    debug

--- frontend/src/reducers/character_diary_reducer.js
+++ frontend/src/reducers/character_diary_reducer.js
@@ -1,15 +1,20 @@
 import { 
     RECEIVE_NEW_ENTRY,
-    RECEIVE_USER_DIARY
+    RECEIVE_USER_DIARY,
+    REMOVE_ENTRY
 } from "../actions/character_diary_actions";   
 
 const characterDiaryReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_NEW_ENTRY:
-            return Object.assign({}, state, action.entry);
+            return Object.assign({}, state, {[Object.keys(state).length]: action.entry});
         case RECEIVE_USER_DIARY:
             return Object.assign({}, action.diary);
+        case REMOVE_ENTRY:
+            const tmpState = Object.assign({}, state);
+            const nextState = Object.values(tmpState).filter(entry => JSON.stringify(entry) !== JSON.stringify(action.entry));
+            return nextState;
         default:
             return state;
     }
