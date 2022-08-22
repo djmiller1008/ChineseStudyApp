72db6cc01ca5e1eb0dc1cc2794751facada7bb6d
David <djmiller1008@yahoo.com>
2022-08-22 23:15:56 +0800

    delete comments

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -6,18 +6,15 @@ export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_USER_SIGN_UP = "RECEIVE_USER_SIGN_UP";
 
-// We'll dispatch this when our user signs in
 export const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
     currentUser
 });
 
-// This will be used to redirect the user to the login page upon signup
 export const receiveUserSignUp = () => ({
     type: RECEIVE_USER_SIGN_UP
 });
   
-// We dispatch this one to show authentication errors on the frontend
 export const receiveErrors = errors => ({
     type: RECEIVE_SESSION_ERRORS,
     errors
@@ -28,7 +25,7 @@ export const logoutUser = () => ({
     type: RECEIVE_USER_LOGOUT
 });
 
-// Upon signup, dispatch the approporiate action depending on which type of response we receieve from the backend
+
 export const signup = user => dispatch => (
     APIUtil.signup(user).then(() => (
         dispatch(receiveUserSignUp())
@@ -37,7 +34,7 @@ export const signup = user => dispatch => (
     ))
 );
 
-// Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
+
 export const login = user => dispatch => (
     APIUtil.login(user).then(res => {
         const { token } = res.data;
