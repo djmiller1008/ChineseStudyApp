eafc09c75d021b72bfc9570ff9cc87d28c61bc19
David <djmiller1008@yahoo.com>
2022-08-23 22:42:41 +0800

    Render a message if the user has no entries

--- frontend/src/components/diary/diary.js
+++ frontend/src/components/diary/diary.js
@@ -3,15 +3,25 @@ import Table from '../table/table';
 
 const Diary = props => {
 
+
     useEffect(() => {
         props.getDiary(props.user_id);
-    }, [])
+    }, [props])
 
-    return (
-        <div>
-            <Table data={props.diary} />
-        </div>
-    )
+    if (props.diary.length > 0) {
+        return (
+            <div>
+                <Table data={props.diary} />
+            </div>
+        )
+    } else {
+        return (
+            <div className="no-entries-div">
+                <p>Go to 'Search' to find characters to add to your diary!</p>
+
+            </div>
+        )
+    }
 }
 
 export default Diary;
