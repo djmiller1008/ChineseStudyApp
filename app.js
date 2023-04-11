bb27a6d72c0c61df3c55401d997877df598cd555
David <djmiller1008@yahoo.com>
2023-04-11 21:04:19 +0800

    Add middleware to allow app to make http call to api

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
